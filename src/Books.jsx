aaeafbedf7499c3f0f8e1e2c1feef148db3d594a
Carlos Marfil <carlos.marfil.r@gmail.com>
2023-07-10 21:30:33 +0100

    added catch book feature and back end feature

--- src/Books.jsx
+++ src/Books.jsx
@@ -5,22 +5,28 @@ import { Link } from "react-router-dom";
 const Book = ({ data }) => {
   return (
     <div className="books">
-      {data.map((book) => (
-        <div className="bubble" key={book.id}>
-          <Link
-            to={`/books/${book.title}`}
-            state={{
-              title: book.title,
-              id: book.id,
-              author: book.Author.name,
-              reason: book.reason,
-              genre: book.Genre.genre,
-            }}
-          >
-            <p className="book-title">{book.title}</p>
-          </Link>
-        </div>
-      ))}
+      {data.map((book) => {
+        if (book.available) {
+          return (
+            <div className="bubble" key={book.id}>
+              <Link
+                to={`/books/${book.title}`}
+                state={{
+                  title: book.title,
+                  id: book.id,
+                  author: book.Author.name,
+                  reason: book.reason,
+                  genre: book.Genre.genre,
+                  available: book.available,
+                }}
+              >
+                <p className="book-title">{book.title}</p>
+              </Link>
+            </div>
+          );
+        }
+        return null;
+      })}
     </div>
   );
 };
--- src/components/BookProfile.jsx
+++ src/components/BookProfile.jsx
@@ -1,12 +1,27 @@
-import React from "react";
-import PageTitle from "../PageTitle";
+import React, { useState } from "react";
+import axios from "axios";
 import { useLocation } from "react-router-dom";
+import PageTitle from "../PageTitle";
 import HomeButton from "./HomeButton";
 
-const BookProfile = () => {
+const BookProfile = ({ readerID }) => {
   const location = useLocation();
-  const { title, author, id, reason, genre } = location.state || {};
-  console.log(author);
+  const { title, author, id, reason, genre, available } = location.state || {};
+  const [isAvailable, setIsAvailable] = useState(available); // New state variable
+
+  const handleCatch = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.patch(`http://localhost:3000/books/${id}`, {
+        available: false,
+        ReaderId: `${readerID}`,
+      });
+      console.log(res, "patch res");
+      setIsAvailable(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <>
@@ -19,6 +34,13 @@ const BookProfile = () => {
           {author},{genre}
         </h2>
         <p>{reason}</p>
+        {isAvailable ? (
+          <button type="button" onClick={handleCatch}>
+            Catch this book!
+          </button>
+        ) : (
+          <p> You have claimed this book! </p>
+        )}
       </div>
     </>
   );
